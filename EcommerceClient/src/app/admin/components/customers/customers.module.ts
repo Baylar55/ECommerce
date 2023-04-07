342642d79c7b16cb80a8f30e0b342a80911d258b
Beyler55 <baylarss@code.edu.az>
2023-04-07 22:27:09 +0400

    Angular multiple layout infrastructure added

--- EcommerceClient/src/app/admin/components/customers/customers.module.ts
+++ EcommerceClient/src/app/admin/components/customers/customers.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from './customers.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,11 @@ import { CustomersComponent } from './customers.component';
     CustomersComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path:"", component:CustomersComponent} 
+      //If this module receives a request called customer(for ex.), it should handle CustomersComponent. If we have more component related with customer, we will declare the path and route of each.If we have only one component we don't need to write path, we can keep empty it.
+    ])
   ]
 })
 export class CustomersModule { }
