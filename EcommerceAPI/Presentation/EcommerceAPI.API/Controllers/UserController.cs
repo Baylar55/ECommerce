150d62d656c260130991a054447fd769e856eaa6
Beyler55 <baylarss@code.edu.az>
2023-05-03 10:18:46 +0400

    Authentication added

--- EcommerceAPI/Presentation/EcommerceAPI.API/Controllers/UserController.cs
+++ EcommerceAPI/Presentation/EcommerceAPI.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using EcommerceAPI.Application.Features.Commands.AppUser.CreateUser;
+using EcommerceAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,13 @@ namespace EcommerceAPI.API.Controllers
         {
             CreateUserCommandResponse response = await _mediator.Send(createUserCommandRequest);
             return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
+            return Ok(response);        
         }
     }
 }
--- EcommerceClient/src/app/services/common/models/user.service.ts
+++ EcommerceClient/src/app/services/common/models/user.service.ts
@@ -17,4 +17,16 @@ export class UserService {
 
     return await firstValueFrom(observable) as Create_User;
   }
+
+  async login(usernameOrEmail: string, password: string, callBackFunction?: ()=> void ): Promise<void>{
+    const observable: Observable<any> = this.httpClientService.post({
+      controller:"user",
+      action:"login" 
+    },{
+      usernameOrEmail, password
+    })
+    return await firstValueFrom(observable);
+    callBackFunction();
+  }
+
 }
