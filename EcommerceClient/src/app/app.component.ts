2d724b8ec4c720bbb2edea805914da0c0b67b3d5
Beyler55 <baylarss@code.edu.az>
2023-05-22 07:59:32 +0400

    Dynamic Component Loading

--- EcommerceClient/src/app/app.component.ts
+++ EcommerceClient/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AuthService } from './services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 import { Router } from '@angular/router';
+import { ComponentType, DynamicLoadComponentService } from './services/common/dynamic-load-component.service';
+import { DynamicLoadComponentDirective } from './directives/common/dynamic-load-component.directive';
 declare var $:any
 @Component({
   selector: 'app-root',
@@ -9,7 +11,11 @@ declare var $:any
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public authService: AuthService, private toastrService:CustomToastrService, private router: Router ) {
+
+  @ViewChild(DynamicLoadComponentDirective, {static: true})
+  dynamicLoadComponentDirective: DynamicLoadComponentDirective;
+
+  constructor(public authService: AuthService, private toastrService:CustomToastrService, private router: Router, private dynamicLoadComponentService: DynamicLoadComponentService ) {
     authService.identityCheck();
   }
   signOut(){
@@ -20,6 +26,12 @@ export class AppComponent {
       messageType: ToastrMessageType.Warning,
       position:ToastrPosition.TopRight
     })
+  };
+
+  loadComponent(){
+
+    this.dynamicLoadComponentDirective.viewContainerRef
+    this.dynamicLoadComponentService.loadComponent(ComponentType.BasketsComponent, this.dynamicLoadComponentDirective.viewContainerRef)
   }
 }
 
